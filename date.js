19edde6bc2e0e911cf90d850db832b2280e8018a
kennyvenny <110496860+kennyvenny@users.noreply.github.com>
2024-03-26 02:21:58 +0300

    задаание 17

--- date.js
+++ date.js
@@ -1,22 +1,9 @@
+import {
+    format
+} from "date-fns";
+
 export function formatDate(date) {
     if (date == undefined) return;
-    let dateNew = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear() - 2000;
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    if (dateNew < 10) {
-        dateNew = "0" + dateNew;
-    }
-    if (month < 10) {
-        month = "0" + month;
-    }
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    return dateNew + "." + month + "." + year + " " + hours + ":" + minutes;
+    
+    return format(date, "yyyy-MM-dd hh.mm.ss");
 };
